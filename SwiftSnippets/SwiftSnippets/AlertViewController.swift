e3d3d0904dbeb52cea10f3341037aa7902d70132
creeptosis <ben.lim@inetsoho.com>
2018-05-10 12:47:03 +0800

    alert

---
+++ SwiftSnippets/SwiftSnippets/AlertViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AlertViewController.swift
+//  SwiftSnippets
+//
+//  Created by Benjamin Lim on 5/10/18.
+//  Copyright © 2018 Creeptosis. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let alert = UIAlertController(title:"title", message:"message", preferredStyle: .alert);
+        
+        //CUSTOM ACTION HANDLER
+        let action1 = UIAlertAction(title: "YES", style: .default, handler: {
+            action in
+            print("YES")
+        })
+        let action2 = UIAlertAction(title:"NO", style: .destructive, handler:  {
+            action in
+            self.doesNothing()
+        })
+        alert.addAction(action1);
+        alert.addAction(action2);
+        
+        present(alert, animated:true, completion:nil);
+    }
+    
+    func doesNothing() { print("literally useless!")}
+}
