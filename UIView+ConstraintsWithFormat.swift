1e22722bded1bc969551cd4d2996c75ee1faaa1b
Creeptosis <creeptosis@gmail.com>
2018-09-28 09:08:47 +0800

    Create UIView+ConstraintsWithFormat.swift

---
+++ UIView+ConstraintsWithFormat.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+AddConstraintsWithFormat.swift
+//  ToDoProgrammatically
+//
+//  Created by Benjamin Lim on 9/28/18.
+//  Copyright © 2018 Creeptosis. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
